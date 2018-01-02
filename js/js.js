c52f14c28dac4a6672ebab21632ef2d5e0b3a1c8
Andrew <veselchak@gmail.com>
2018-01-02 20:47:37 +0200

    Page 3 sliders final

--- js/js.js
+++ js/js.js
@@ -1,4 +1,6 @@
 $('#fullpage').fullpage({
     anchors: ['Page1', 'Page2', 'Page3', 'Page4', 'Page5', 'Page6', 'Page7'],
-    menu: '#meinMenu'
+    menu: '#meinMenu',
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
 });
