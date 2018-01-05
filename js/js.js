ef36f618d5e8d814f0fcb8b52914ec9fdbaf5a59
Andrew <veselchak@gmail.com>
2018-01-05 19:11:12 +0200

    Add buttons

--- js/js.js
+++ js/js.js
@@ -4,3 +4,20 @@ $('#fullpage').fullpage({
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
 });
+
+// button scr
+(function() {
+    var removeSuccess;
+
+    removeSuccess = function() {
+        return $('.button').removeClass('success');
+    };
+
+    $(document).ready(function() {
+        return $('.button').click(function() {
+            $(this).addClass('success');
+            return setTimeout(removeSuccess, 3000);
+        });
+    });
+
+}).call(this);
